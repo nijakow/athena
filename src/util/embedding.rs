0a049827501bda7889ac4a560ee8aa64b835004c
nijakow <79372954+nijakow@users.noreply.github.com>
2025-03-21 12:52:04 +0100

    Changed the embedding mechanism

--- src/util/embedding.rs
+++ src/util/embedding.rs
@@ -1,5 +1,7 @@
-use crate::core::{entity::{self, zettel::document::conversions::html::HtmlConversionContext}, io::resource};
-
+use crate::core::{
+    entity::{self, zettel::document::conversions::html::HtmlConversionContext},
+    io::resource,
+};
 
 fn content_not_displayed() -> maud::Markup {
     maud::html! {
@@ -7,7 +9,28 @@ fn content_not_displayed() -> maud::Markup {
     }
 }
 
-pub fn embed_file_for_id<S: ToString>(file: &entity::file::File, id: &entity::Id, title: S, download: bool) -> maud::PreEscaped<String> {
+fn decorate_with_link_button<S: ToString>(
+    uri: S,
+    content: maud::PreEscaped<String>,
+) -> maud::Markup {
+    // Create a div element that overlays a link button at the top right corner of the embedded content. Inline the CSS.
+
+    let uri = uri.to_string();
+
+    maud::html! {
+        div style="position: relative" {
+            (content)
+            a href=(uri) style="position: absolute; top: 0; right: 0; padding: 0.5em; background-color: #000; color: #fff; text-decoration: none" { "Open" }
+        }
+    }
+}
+
+pub fn embed_file_for_id<S: ToString>(
+    file: &entity::file::File,
+    id: &entity::Id,
+    title: S,
+    download: bool,
+) -> maud::PreEscaped<String> {
     let title = title.to_string();
     let mime = file.metadata().mime_type().to_string();
 
@@ -20,7 +43,7 @@ pub fn embed_file_for_id<S: ToString>(file: &entity::file::File, id: &entity::Id
     match file.metadata().file_type() {
         resource::Type::Document(resource::DocumentType::PlainText) => {
             if let Ok(content) = std::str::from_utf8(file.content()) {
-               maud::html! {
+                maud::html! {
                     pre { (content) }
                 }
             } else {
@@ -28,9 +51,17 @@ pub fn embed_file_for_id<S: ToString>(file: &entity::file::File, id: &entity::Id
             }
         }
         resource::Type::Document(resource::DocumentType::Pdf) => {
-            // Make sure that the PDF has a certain height, it should not be crushed vertically
-            maud::html! {
-                object class="pdf" data=(uri) type="application/pdf" width="100%" style="aspect-ratio: 4 / 3" {}
+            let content = maud::html! {
+                // Keep the link to the original URI in the tag, so that the PDF can be opened in a new tab
+                object class="pdf" data=(id.as_safe_download_uri()) type="application/pdf" width="100%" style="aspect-ratio: 4 / 3" {}
+            };
+            
+            if download {
+                content
+            } else {
+                maud::html! {
+                    (decorate_with_link_button(uri, content))
+                }
             }
         }
         resource::Type::Image(_) => {
@@ -49,7 +80,11 @@ pub fn embed_file_for_id<S: ToString>(file: &entity::file::File, id: &entity::Id
     }
 }
 
-pub fn embed_entity_for_id(entity: &entity::Entity, id: &entity::Id, conversion_context: &HtmlConversionContext) -> maud::PreEscaped<String> {
+pub fn embed_entity_for_id(
+    entity: &entity::Entity,
+    id: &entity::Id,
+    conversion_context: &HtmlConversionContext,
+) -> maud::PreEscaped<String> {
     use entity::zettel::document::conversions::html::AsHtml;
 
     match entity {
