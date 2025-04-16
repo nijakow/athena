02147e5db98ebf29ffb33b84df698f396899d178
nijakow <79372954+nijakow@users.noreply.github.com>
2025-04-16 15:06:03 +0200

    Implemented a very basic metadata info box

--- src/web/pages/content/zettel.rs
+++ src/web/pages/content/zettel.rs
@@ -6,6 +6,36 @@ use maud::html;
 use crate::{core::{entity::{self, zettel::{self, document::conversions::html::AsHtml}}, vault}, web::pages};
 
 
+/// Generate a HTML table containing the metadata of a zettel.
+fn generate_metadata_box(
+    header: &zettel::Header,
+) -> maud::PreEscaped<String> {
+    let yaml = &header.yaml;
+
+    match yaml {
+        Some(yaml) => {
+            fn yaml_to_string(yaml: &yaml_rust2::Yaml) -> String {
+                let mut buffer = String::new(); // Create a buffer to write the serialized YAML
+                let mut emitter = yaml_rust2::YamlEmitter::new(&mut buffer); // Create a YamlEmitter
+                emitter.dump(yaml).unwrap(); // Serialize the YAML into the buffer
+                buffer // Use the buffer directly as it is already a String
+            }
+
+            let yaml_string = yaml_to_string(yaml);
+
+            maud::html! {
+                pre {
+                    code {
+                        (yaml_string)
+                    }
+                }
+            }
+        }
+        None => maud::PreEscaped(String::new()),
+    }
+}
+
+
 pub fn generate_show_zettel(
     vault: &Arc<vault::Vault>,
     id: entity::Id,
@@ -26,6 +56,8 @@ pub fn generate_show_zettel(
         html! {
             h1 { (title) }
             a href=(format!("{}?action=edit", id.as_safe_uri())) { "Edit" }
+            br;
+            (generate_metadata_box(&zettel.header))
             br;
             (maud::PreEscaped(content))
         },
